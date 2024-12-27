a4d236a36393a6373f4c1821cbf345d422f90df8
Akshay Kumar Jain <akshayjain813@gmail.com>
2024-12-27 15:40:22 +0530

    Add basic logic in add method

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,3 +1,9 @@
 class Calculator
+  class << self
+    def add(numbers)
+      return 0 if numbers.empty?
 
+      return numbers.to_i
+    end
+  end
 end
--- spec/lib/calculator_spec.rb
+++ spec/lib/calculator_spec.rb
@@ -1,6 +1,21 @@
-require_relative '../lib/calculator'
+require_relative '../../lib/calculator'
 
 describe Calculator do
   describe 'calculator logics' do
+    context '.add' do
+      context 'when empty string is passed' do
+        it 'should returns 0 value' do
+          expect(described_class.add('')).to eq(0)
+        end
+      end
+  
+      context 'when string is not empty' do
+        context 'when string contains single number available in the passed string' do
+          it 'returns should return the correct number' do
+            expect(described_class.add('7')).to eq(7)
+          end
+        end
+      end
+    end
   end
 end
